46b653d454ae94396c8b5c1654ef6eaeec5fbf70
Ahmed Ibrahem <66860955+AI091@users.noreply.github.com>
2023-07-07 03:31:44 +0300

    Added UI for notifications using react-toastify

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -1,22 +1,19 @@
-// Import required packages and components
 import { useState, useEffect } from 'react';
 import { collection, query, where, onSnapshot, doc, updateDoc } from 'firebase/firestore';
-
 import { db } from '../../config/firebase';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// Function to request notification permission
 async function requestNotificationPermission() {
   const permission = await Notification.requestPermission();
   return permission === 'granted';
 }
 
-// Function to mark notification as seen
 async function markNotificationAsSeen(notificationId: string) {
   const notificationRef = doc(db, 'notifications', notificationId);
   await updateDoc(notificationRef, { seen: true });
 }
 
-// NotificationsComponent
 const NotificationsComponent = () => {
   const [notifications, setNotifications] = useState<any>([]);
 
@@ -31,11 +28,13 @@ const NotificationsComponent = () => {
           const unreadNotifications: any = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
           setNotifications(unreadNotifications);
           unreadNotifications.forEach((notification: { id: string; content: any; }) => {
-            const browserNotification = new Notification(notification.id, { body: notification.content });
-            browserNotification.onclick = async () => {
-              await markNotificationAsSeen(notification.id);
-              setNotifications(notifications.filter((notif: { id: string; }) => notif.id !== notification.id));
-            };
+            const toastId = toast(notification.content, {
+              onClick: async () => {
+                await markNotificationAsSeen(notification.id);
+                setNotifications(notifications.filter((notif: { id: string; }) => notif.id !== notification.id));
+                toast.dismiss(toastId);
+              },
+            });
           });
         });
 
@@ -47,10 +46,10 @@ const NotificationsComponent = () => {
     fetchData();
   }, []);
 
-  // Render the component (not displaying notifications in the UI)
   return (
     <div>
       {/* Your other components and UI elements */}
+      <ToastContainer />
     </div>
   );
 };
